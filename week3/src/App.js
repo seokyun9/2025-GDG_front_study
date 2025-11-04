a1ee25b7c5976467a86a4e138877558a0b67709e
seokyun9 <westse05@gmail.com>
2025-11-04 17:20:54 +0900

    submit: week 3

---
+++ week3/src/App.js
@@ -0,0 +1,23 @@
+import './App.css';
+//import Dog from './family/Dog';
+//import Father from './family/Father';
+//import Me from './family/Me';
+import Introduction from './introduction/Introduction';
+import Sub from './introduction/sub';
+
+function App() {
+
+ return (
+
+ <div className="App">
+    <Introduction />
+    <Sub />
+ </div>
+
+
+ );
+
+}
+
+
+export default App;
---
+++ week3/src/introduction/Introduction.js
@@ -0,0 +1,24 @@
+function Introduction() {
+
+ return (
+
+ <div>
+
+ <h1>자기소개</h1>
+
+ <p> 안녕하세요 한서경입니다!
+    GDG hongik DevRel 팀 코어멤버에요. </p>
+
+<h2> Hongik Univ. </h2>
+    <p>
+        Major in Liberal Studies
+    </p>
+
+ </div>
+
+ );
+
+}
+
+
+export default Introduction;
